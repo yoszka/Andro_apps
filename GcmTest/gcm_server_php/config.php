f4e87e652f8723cfe0961dfdb17c9e8d778dcee8
root <root@jokii.(none)>
2013-03-22 23:34:48 +0100

    Add Test project concern Google Cloud Messaging (GCM)

---
+++ GcmTest/gcm_server_php/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", 	"localhost");
+define("DB_USER", 	"root");
+define("DB_PASSWORD", 	"android");
+define("DB_DATABASE", 	"gcm");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyA3DeMKrdtb6gG4kYdnbHbm-JUvm1ouikg"); // Place your Google API Key
+?>
