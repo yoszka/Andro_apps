c319887247bcd79c7d5f749400a1a800addfd8c5
Tomasz Jokiel <tomasz.jokiel@gmail.com>
2013-05-30 14:56:38 +0200

    EventAnnouncement: Updated announceEvent script to handle registration id from index

--- EventAnnouncement/announceEvent_HOST/announceEvent.sh
+++ EventAnnouncement/announceEvent_HOST/announceEvent.sh
@@ -22,16 +22,36 @@ location_timestamp=1363388511000
 
 
 
-#curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"location_lat\":\"$location_lat\",\"location_lon\":\"$location_lon\",\"location_acc\":\"$location_acc\",\"location_provider\":\"$location_provider\",\"location_timestamp\":\"$location_timestamp\"}}"
+
 
 #curl -s  http://pinnote.zz.mu/list_all.php | grep -Po '"id":"15","gcm_regid":"(.*)' | grep -Po 'gcm_regid":"(.*)' | grep -Po '[^:"]{20,}'
 registration_id=`curl -s  http://pinnote.zz.mu/list_all.php | grep -Po '"id":"'$1'","gcm_regid":"(.*)' | grep -Po 'gcm_regid":"(.*)' | grep -Po '[^:"]{20,}' | head -1`
 #aaaaa='"id":"'$1'","gcm_regid":"(.*)'
 #echo $aaaaa
-#echo $registration_id
+echo $registration_id
+
+# to check if LocationReceiver works
+#curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"location_lat\":\"$location_lat\",\"location_lon\":\"$location_lon\",\"location_acc\":\"$location_acc\",\"location_provider\":\"$location_provider\",\"location_timestamp\":\"$location_timestamp\"}}"
+#exit
 
 token=event
+#ls -la | tail -c 4000 > some_file.txt
+message='Hello World\n===========\n'
 message=`ls -la | tail -c 4000`
+message=`cat some_file.txt`
 #message=`tail -n 4 some_file.txt`
 # added print no output `-silent -output`
-curl -silent -output --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"message\":\"$message\"}}" 
+#curl -silent -output --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"message\":\"`cat -v some_file.txt`\"}}"
+#curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"message\":\"`echo \"$(<some_file.txt)\"`\"}}" 
+#curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"message\":\"$message\"}}" 
+
+
+#zmienna="{\"registration_ids\":[\"$registration_id\"],\"data\":{\"token\":\"$token\",\"message\":\"`echo -ne \"$message\"`\"}}"
+
+#curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  -d "`echo -ne \"$zmienna\"`" 
+#curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  --data-binary "$(<tmp_file.txt)" 
+curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send   --data-binary  @tmp_file.txt
+# curl --header "Authorization: key=$api_key" --header Content-Type:"application/json" https://android.googleapis.com/gcm/send  --data-binary $"`echo -ne \"$zmienna\"`" 
+
+#echo -ne "$zmienna" 
+#echo -ne "$message"
